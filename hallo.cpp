5bede07e4f3e95cb60b57dc82f35922732becf0b
Manyullyn <manuel.greiler@gmail.com>
2021-10-01 12:52:44 +0200

    added cout

--- hallo.cpp
+++ hallo.cpp
@@ -1,2 +1,12 @@
-//mein hallo-programm
- // (c)manuel.greiler@gmail.com 2021
+// mein hallo-programm
+// (c)manuel.greiler@gmail.com 2021
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello People of the Internet!" << endl;
+	return 0;
+}
